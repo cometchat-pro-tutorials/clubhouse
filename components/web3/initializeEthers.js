f98fd164d2438625ad9c402acf505f38f6ff9f38
Jomari Peterson <jomari.peterson@gmail.com>
2024-02-17 20:13:15 -0800

    Social Keys working on movement

--- components/web3/initializeEthers.js
+++ components/web3/initializeEthers.js
@@ -4,12 +4,12 @@ import socialKeysABI from '../../abi/socialkey.json';
 // Import ABI and contract addresses as needed
 
  // Assuming these are the known network parameters
-  //const rpcUrl = "https://mevm.devnet.m1.movementlabs.xyz/v1";
-  const rpcUrl= "https://api.avax.network/ext/bc/C/rpc";
+  const rpcUrl = "https://mevm.devnet.m1.movementlabs.xyz/v1";
+  //const rpcUrl= "https://api.avax.network/ext/bc/C/rpc";
   //const chainId = 336; // The chain ID for your custom network
 
-   //const contractAddress = '0xf7333e9fb03f25088879005fdCA9406993f33878'; //M1 Social Keys
-  const contractAddress = '0xfedA2BAE8F800E990fF3f0848eBd7Eb24b4f6408'; //Avax Social Keys (hottakes.io)
+   const contractAddress = '0xf7333e9fb03f25088879005fdCA9406993f33878'; //M1 Social Keys
+ // const contractAddress = '0xfedA2BAE8F800E990fF3f0848eBd7Eb24b4f6408'; //Avax Social Keys (hottakes.io)
 
 export const initializeEthers = async () => {
 
